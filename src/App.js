e193da1f2feabe7eae4727f7835e6e12e97edda9
Ali Afzal <aliafzal6111@gmail.com>
2021-01-10 23:56:49 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          <br>Edited By Ali</br>
+          <br>...</br>
+          Edited By Ali
         </p>
         <a
           className="App-link"
